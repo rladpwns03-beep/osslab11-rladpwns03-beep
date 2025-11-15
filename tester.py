9241a2109c425d381c93388f4f9daa387bf2e5a1
rladpwns03-beep <rladpwns03@gachon.ac.kr>
2025-11-15 10:35:19 +0900

    Fix print formatting for hypotenuse output

--- tester.py
+++ tester.py
@@ -3,7 +3,7 @@ import geo.utils as utils
 # calculate the length of hypoteneuse(c) when a=3 and b=4
 a, b = 3, 4
 c = utils.pythagoras(a,b)
-print('c=', c)
+print('c =', c)
 
 # callculate the area of circle with radius r = 10
 r = 10
